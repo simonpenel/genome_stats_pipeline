14fd6a0c33d0ef060cdda6c6bfb934252012f408
Simon Penel <simon.penel@univ-lyon1.fr>
2024-03-04 11:01:57 +0100

    Fandling case when hmmsearch give the samve value to start and end

--- scripts/step_stats/python/blastp_analysis.py
+++ scripts/step_stats/python/blastp_analysis.py
@@ -33,27 +33,30 @@ with open(f"results/{accession}/blastp.txt", 'w') as writer:
             zf = row['Nb ZF domains']
         prot = f"<\t{set}\t{krab}\t{ssxrd}\t{zf}\n"
         print(f"Run blastdbcmd -db data/ncbi/{accession}/protdb -entry {row['SeqID']} -range {int(row['SET domain start'])}-{int(row['SET domain end'])} -out data/ncbi/{accession}/SET_sequences/{row['SeqID']}.fa")
-        ret = os.system(f"blastdbcmd -db data/ncbi/{accession}/protdb -entry {row['SeqID']} -range {int(row['SET domain start'])}-{int(row['SET domain end'])} -out data/ncbi/{accession}/SET_sequences/{row['SeqID']}.fa")
-        if ret > 0 :
-            sys.exit("Error during blastdbcmd")
-        print(f"Run blastp -db data/PRDM_family_HUMAN/prdm_family -outfmt 7 -query data/ncbi/{accession}/SET_sequences/{row['SeqID']}.fa -out data/ncbi/{accession}/SET_blastp/{row['SeqID']}")
-        ret = os.system(f"blastp -db data/PRDM_family_HUMAN/prdm_family -outfmt 7 -query data/ncbi/{accession}/SET_sequences/{row['SeqID']}.fa -out data/ncbi/{accession}/SET_blastp/{row['SeqID']}")
-        if ret > 0 :
-            sys.exit("Error during blastp")
-        with open(f"data/ncbi/{accession}/SET_blastp/{row['SeqID']}") as reader:
-            prot_id = row['SeqID']
-            lines = reader.readlines()
-            prdm_match = lines[5].split()[1].split('_')[0]
-            df.at[index, 'Best Match'] = lines[5].split()[1].split('_')[0] # Best match Prdm number
-            if prdm_match == 'PRDM9': # if it is prdm9, save the score and compare it to the next non-prdm9 best match
-                df.at[index, 'Bit Score'] = float(lines[5].split()[-1])
-                j = 1
-                while lines[5 + j].split()[1].split('_')[0] == 'PRDM9': 
-                    j += 1
-                df.at[index, 'Score ratio'] = float(lines[5].split()[-1])/float(lines[5 + j].split()[-1])
-                string += f"{prot}{prot_id}\t{float(lines[5].split()[-1])}\t{float(lines[5].split()[-1])/float(lines[5 + j].split()[-1])}\t{prdm_match}\n"
-            else:
-                string += f"{prot}{prot_id}\t{prdm_match}\n"
+        if int(row['SET domain end'] > int(row['SET domain start'] :
+            ret = os.system(f"blastdbcmd -db data/ncbi/{accession}/protdb -entry {row['SeqID']} -range {int(row['SET domain start'])}-{int(row['SET domain end'])} -out data/ncbi/{accession}/SET_sequences/{row['SeqID']}.fa")
+            if ret > 0 :
+                sys.exit("Error during blastdbcmd")
+            print(f"Run blastp -db data/PRDM_family_HUMAN/prdm_family -outfmt 7 -query data/ncbi/{accession}/SET_sequences/{row['SeqID']}.fa -out data/ncbi/{accession}/SET_blastp/{row['SeqID']}")
+            ret = os.system(f"blastp -db data/PRDM_family_HUMAN/prdm_family -outfmt 7 -query data/ncbi/{accession}/SET_sequences/{row['SeqID']}.fa -out data/ncbi/{accession}/SET_blastp/{row['SeqID']}")
+            if ret > 0 :
+                sys.exit("Error during blastp")
+            with open(f"data/ncbi/{accession}/SET_blastp/{row['SeqID']}") as reader:
+                prot_id = row['SeqID']
+                lines = reader.readlines()
+                prdm_match = lines[5].split()[1].split('_')[0]
+                df.at[index, 'Best Match'] = lines[5].split()[1].split('_')[0] # Best match Prdm number
+                if prdm_match == 'PRDM9': # if it is prdm9, save the score and compare it to the next non-prdm9 best match
+                    df.at[index, 'Bit Score'] = float(lines[5].split()[-1])
+                    j = 1
+                    while lines[5 + j].split()[1].split('_')[0] == 'PRDM9': 
+                        j += 1
+                    df.at[index, 'Score ratio'] = float(lines[5].split()[-1])/float(lines[5 + j].split()[-1])
+                    string += f"{prot}{prot_id}\t{float(lines[5].split()[-1])}\t{float(lines[5].split()[-1])/float(lines[5 + j].split()[-1])}\t{prdm_match}\n"
+                else:
+                    string += f"{prot}{prot_id}\t{prdm_match}\n"
+        else :
+            print(f"blastdbcmd was not processed")        
     df.to_csv(f"results/{accession}/summary_table_{accession}.csv", sep=';')
     if taxid == None:
         print("Nothing found in blastp")
